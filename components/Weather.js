59408868a5f51fcb8562f571828e801f86d5915a
Aekkasitrun <60956669+Aekkasitrun@users.noreply.github.com>
2020-08-29 19:25:56 +0700

    เพิ่มZipCode จัดให้อยู่ตรงกลาง

--- components/Weather.js
+++ components/Weather.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Text } from 'react-native'
+import { Text, ImageBackground, StyleSheet } from 'react-native'
 import Forecast from './Forecast'
 
 export default function Weather(props) {
@@ -10,8 +10,20 @@ export default function Weather(props) {
     })
     
     return (
+        <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
+            <Text>Zip Code</Text>
+            <Text>{props.zipCode}</Text>
+           <Forecast {...forecastInfo}/> 
+        </ImageBackground>
         
-        <Forecast {...forecastInfo}/>
-    );
+    )
    }
+
+   const styles = StyleSheet.create({
+       backdrop: {
+           alignItems: 'center',
+           width: '100%',
+           height: '100%'
+       }
+   })
    
