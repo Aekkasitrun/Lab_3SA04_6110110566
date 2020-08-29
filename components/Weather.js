36b8b5d38f0d9eb6121cf7a9189522669fb34dc9
Aekkasitrun <60956669+Aekkasitrun@users.noreply.github.com>
2020-08-29 19:34:39 +0700

    part4

--- components/Weather.js
+++ components/Weather.js
@@ -21,9 +21,11 @@ export default function Weather(props) {
 
    const styles = StyleSheet.create({
        backdrop: {
-           alignItems: 'center',
-           width: '100%',
-           height: '100%'
+        
+            flexDirection: 'column',
+            alignItems: 'center',
+            width: '100%',
+            height: '100%'
        }
    })
    
