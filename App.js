e4dfcfdc619b1b16f71c2720d8c81900cca35ed5
Aekkasitrun <60956669+Aekkasitrun@users.noreply.github.com>
2020-08-29 16:42:54 +0700

    weather2

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weather';
 export default function App() {
  const doIt = () => {
  console.log("Hello from console")
