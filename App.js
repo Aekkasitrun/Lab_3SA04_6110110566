1ec9b20c8240f81fad08fd8034ae185fa789ba41
Aekkasitrun <60956669+Aekkasitrun@users.noreply.github.com>
2020-08-29 16:21:05 +0700

    2

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
   
   return (
     <View style={styles.container}>
-      <Text onPress={doIt}>Hello</Text>
+      <Text onPress={doIt}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
